395194b501a14b3ca91227a2825a430c59ab857c
ytnobody <ytnobody@gmail.com>
2012-08-10 17:09:41 +0900

    initial commit

---
+++ lib/MagicBullet.pm
@@ -0,0 +1,32 @@
+package MagicBullet;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+MagicBullet -
+
+=head1 SYNOPSIS
+
+  use MagicBullet;
+
+=head1 DESCRIPTION
+
+MagicBullet is
+
+=head1 AUTHOR
+
+ytnobody E<lt>ytnobody@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
